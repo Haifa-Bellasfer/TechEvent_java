76fade2fc7508e66ccf7a55432db6a0434af68cd
khaledmrad <47251343+khaledmrad@users.noreply.github.com>
2019-04-17 14:39:34 +0100

    modif

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/view/Home.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/AdminDashborad.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setResizable(false);
